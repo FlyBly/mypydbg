3d2d82cdb32ea135015698228fa3601c610c574c
FlyBly <421405945@qq.com>
2017-05-09 08:26:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,8 @@ import debugger
 
 debug = debugger.debugger()
 
-# B:\\FileCleaner2.0.exe
-#exepath = raw_input("Enter Exe path:")
+exepath = raw_input("Enter Exe path:")
 
-exepath ="B:\\FileCleaner2.0.exe"
 #执行exe文件
 debug.load(exepath)
 
